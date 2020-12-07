acbbaafe4008ca823bab756eb8a58fed8252190b
Kristof Keseru <kristofkeseru1117@gmail.com>
2020-12-07 16:59:20 +0100

    Created del function with use of spread operator and delete keyword

--- plus/oop/delete.js
+++ plus/oop/delete.js
@@ -7,3 +7,13 @@
  * @param {string} key the name of the property to delete
  * @returns {object} the new object without the given property
  */
+function del(o, key) {
+    if (typeof o != "object") {
+        throw new Error("Not object is passed");
+    }
+    let o2 = {...o};
+    delete o2[key];
+    return o2;
+}
+
+module.exports = del;
