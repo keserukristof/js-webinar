a6cda6804cdf24a6c6b506ffa305cef924521234
Kristof Keseru <kristofkeseru1117@gmail.com>
2020-12-08 10:59:06 +0100

    Implemented the HomePage class

--- module-3/pop/HomePage.js
+++ module-3/pop/HomePage.js
@@ -3,3 +3,29 @@
  * Add main widgets and element of the page and write tests
  * for it (test/pop/HomePage.spec.js).
  */
+const Layout = require("./Layout")
+const ElementFinder = require("../test/mock/ElementFinder");
+
+class HomePage extends Layout{
+    constructor(name, url, locator) {
+        super(name, url, locator);
+    }
+
+    getHeader() {
+        if (this.children.hasOwnProperty("Header")) {
+            return new ElementFinder(this.children["Header"].locator);
+        } else {
+            throw new Error("Header is not found!");
+        }
+    }
+
+    getSlider(parentElement) {
+        if (this.children.hasOwnProperty("Slider")) {
+            return new ElementFinder(this.children["Slider"].locator);
+        } else {
+            throw new Error("Slider is not found!");
+        }
+    }
+}
+
+module.exports = HomePage;
