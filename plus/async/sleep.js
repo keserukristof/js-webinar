8760d9831b6b8201952bdbae0b710fc248b8b18c
Kristof Keseru <kristofkeseru1117@gmail.com>
2021-01-06 17:07:51 +0100

    Implemented the wait function

--- plus/async/sleep.js
+++ plus/async/sleep.js
@@ -2,6 +2,18 @@
  * It wait for the given seconds, but at most 10 seconds.
  *
  * @param {number} sec the seconds to wait
- * @returns {Promise} the promise which resolves when wait if over
+ * @returns {Promise} the promise which resolves when wait is over
  *                    or rejects if parameter is not correct
  */
+module.exports = function wait(sec) {
+    return new Promise((resolve, reject) => {
+        if (typeof sec === "number" && sec > 1) {
+            if (sec > 10) {
+                sec = 10;
+            }
+            setTimeout(resolve, sec * 1000);
+        } else {
+            reject();
+        }
+    });
+}
