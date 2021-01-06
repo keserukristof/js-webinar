ff26d071afad443ccecf44e225709fee797ae9dc
Kristof Keseru <kristofkeseru1117@gmail.com>
2021-01-06 17:03:51 +0100

    Removed the 'else' and the 'await' from the if else

--- plus/async/loadJson.js
+++ plus/async/loadJson.js
@@ -2,19 +2,11 @@
  * Rewrite the following code to use async/await 
  */
 module.exports = async function loadJson(url) {
-    // return fetch(url)
-    //     .then(response => {
-    //         if (response.status == 200) {
-    //             return response.json();
-    //         } else {
-    //             throw new Error(response.status);
-    //         }
-    //     });
     const response = await fetch(url);
 
     if (response.status == 200) {
-        return await response.json();
-    } else {
-        throw new Error(response.status);
+        return response.json();
     }
+
+    throw new Error(response.status);
 };
