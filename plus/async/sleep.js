2fe86c91d578e57d55482a347a62d3575e92b7fd
Kristof Keseru <kristofkeseru1117@gmail.com>
2021-01-08 11:44:13 +0100

    Reject the the 'sec > 10' case in the wait function

--- plus/async/sleep.js
+++ plus/async/sleep.js
@@ -8,13 +8,13 @@
 module.exports = function wait(sec) {
     return new Promise((resolve, reject) => {
         if (typeof sec !== "number") {
-            reject(Error("sec has to be a number"));
+            reject("sec has to be a number");
         }
         if (sec < 1) {
-            reject(Error("sec can not be less than 1"));
+            reject("sec can not be less than 1");
         }
         if (sec > 10) {
-            sec = 10;
+            reject("sec can not be bigger than 10")
         }
         setTimeout(() => resolve(), sec * 1000);
     });
