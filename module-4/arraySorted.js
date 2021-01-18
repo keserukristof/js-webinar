cf95274077b74322f4bf8bf19e45c00fb48e8ff1
Kristof Keseru <kristofkeseru1117@gmail.com>
2021-01-18 14:24:31 +0100

    Implemented the arraySorted function

--- module-4/arraySorted.js
+++ module-4/arraySorted.js
@@ -12,3 +12,24 @@
  * @returns {boolean} true if the array is properly sorted,
  *                    false otherwise
  */
+function arraySorted(items) {
+    const clearItems = items.map(item => {
+        if (typeof item === "string") {
+            return item.replace(/[^a-zA-Z]/g, "").toLowerCase();
+        } else {
+            return item;
+        }
+    });
+
+    const sortedCleanItems = clearItems.slice().sort()
+
+    for (let i = 0; i < items.length; i++) {
+        if (clearItems[i] !== sortedCleanItems[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+module.exports = arraySorted;
