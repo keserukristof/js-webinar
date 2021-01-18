7ee3faae0e5b157c697d3c2e620fa4cb8c046779
Kristof Keseru <kristofkeseru1117@gmail.com>
2021-01-18 14:25:45 +0100

    Implemented the toCamelCase function

--- module-4/toCamelCase.js
+++ module-4/toCamelCase.js
@@ -5,3 +5,17 @@
  * @param {string} toConvert
  * @returns {string} camel-case string or empty string in other cases
  */
+function toCamelCase(toConvert) {
+    if (typeof toConvert !== "string") {
+        return "";
+    }
+
+    return toConvert.trim()
+                    .replace(/[^a-zA-Z" 1-9]/g, "")
+                    .replace(/(?:^\w|[A-Z]|\b\w)/g, (ltr, idx) => {
+                        return idx === 0 ? ltr.toLowerCase() : ltr.toUpperCase();
+                    })
+                    .replace(/\s+/g, '');
+}
+
+module.exports = toCamelCase;
