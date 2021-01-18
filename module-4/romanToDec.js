3b57906577b952477a4dcefa8d1379a62f9a2684
Kristof Keseru <kristofkeseru1117@gmail.com>
2021-01-18 14:25:32 +0100

    Implemented the romanToDec function

--- module-4/romanToDec.js
+++ module-4/romanToDec.js
@@ -7,3 +7,46 @@
  * @returns {number} the Arabic (decimal) equivalent of the parameter
  * @throws Error in case of invalid input
  */
+function romanToDecimal(roman) {
+    if (typeof roman !== "string") {
+        throw new Error("Not valid input. Please give a string as parameter.");
+    }
+
+    const romanDecimalPairs = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    };
+
+    for (let i = 0; i < roman.length; i++) {
+        if (!Object.keys(romanDecimalPairs).includes(roman[i])) {
+            throw new Error(`The ${roman[i]} at the ${i + 1} character is not valid.`);
+        }
+        if (roman[i + 3] === roman[i + 2] && roman[i + 2] === roman[i + 1] && roman[i + 1] === roman[i]) {
+            throw new Error("A roman number can not contains four or more of the same character.")
+        }
+    }
+
+
+    let sum = 0;
+    let currentValue;
+    let nextValue;
+
+    for (let i = 0; i < roman.length; i++) {
+        currentValue = romanDecimalPairs[roman[i]];
+        nextValue = romanDecimalPairs[roman[i + 1]];
+
+        if (currentValue < nextValue) {
+            sum -= currentValue;
+        } else {
+            sum += currentValue;
+        }
+    }
+    return sum;
+}
+
+module.exports = romanToDecimal;
