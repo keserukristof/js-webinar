d68e8239cf76d8d029db7a0338bc1f6834959886
Kristof Keseru <kristofkeseru1117@gmail.com>
2021-02-18 11:00:16 +0100

    Implemented the unit tests for the calc class.

--- module-2/test/calc.spec.js
+++ module-2/test/calc.spec.js
@@ -17,5 +17,53 @@ describe.only('calc', () => {
      *     .times(6).v // 24
      */
     // TODO: write test cases to test calculator
+    it("should return a number", () => {
+        return expect(calc(3).v).to.be.a('number');
+    });
 
+    it("should return the parameter", () => {
+        return expect(calc(3).v).to.equal(3);
+    });
+
+    it("should return the sum of the input numbers", () => {
+        return expect(calc(3).add(5).v).to.equal(8);
+    });
+
+    it("should return the difference between the input numbers", () => {
+        return expect(calc(3).minus(2).v).to.equal(1);
+    });
+
+    it("should return the root of the input number", () => {
+        return expect(calc(4).sqrt().v).to.equal(2);
+    });
+
+    it("should throw error if negative number passed", () => {
+        return expect(() => calc(-1).sqrt().to.throw());
+    });
+
+    it("should return the calc input number multiplied by the times input number", () => {
+        return expect(calc(3).times(10).v).to.equal(30);
+    });
+
+    it("should return the calc input number divided by the divide input number", () => {
+        return expect(calc(10).divide(2).v).to.equal(5);
+    });
+
+    it("should throw error if the divide input number is 0", () => {
+        return expect(() => calc(10).divide(0).to.throw());
+    });
+
+    it("should return the calc input mod modulo input number", () => {
+        return expect(calc(10).modulo(5).v).to.equal(0);
+    });
+
+    it("should return the calc input mod modulo input number", () => {
+        return expect(calc(10).modulo(5).v).to.equal(0);
+    });
+
+    it("should return the expected result of the calculation", () => {
+        return expect(calc(3).add(4)
+                                .minus(3)
+                                .times(6).v).to.equal(24);
+    });
 });
