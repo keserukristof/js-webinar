c954c414889964fbc567b2dbe26b39b1486be098
Kristof Keseru <kristof_keseru@epam.com>
2021-05-09 19:11:25 +0200

    Test js-webinar-test-on-push 15

--- module-1/classification.js
+++ module-1/classification.js
@@ -24,6 +24,7 @@
  * Test js-webinar-test-on-push12
  * Test js-webinar-test-on-push13
  * Test js-webinar-test-on-push14
+ * Test js-webinar-test-on-push15
  */
 function grade(score) {
     let gradeOfStudent;
