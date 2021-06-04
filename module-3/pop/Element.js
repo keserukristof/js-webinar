391e6008c0da6df9956c8dc2a274969d25918787
Kristof Keseru <kristof_keseru@epam.com>
2021-06-04 16:34:29 +0200

    Removed comment from Element.js

--- module-3/pop/Element.js
+++ module-3/pop/Element.js
@@ -14,7 +14,6 @@
  *       in it's children (recursively) the Element with
  *       the given name or throws an Erorr if it cannot
  *       find the element
- *       Test jenkins 23
  */
 const { element } = require("../test/mock/ElementFinder");
 
